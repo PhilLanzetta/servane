c3f72eb009566a4621f767eb58e29a98d2f2dbdb
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2022-09-12 12:40:46 -0400

    slideshow

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,41 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./header"
+import SlideShow from "./slideshow"
 
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [idleTime, setIdleTime] = useState(0)
+
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
+
+  useEffect(() => {
+    if (idleTime < 9) {
+      const timer = setTimeout(() => {
+        setIdleTime(idleTime + 1)
+      }, 1000)
+      return () => clearTimeout(timer)
+    }
+  }, [idleTime])
+
   return (
     <>
       <Header toggleMenu={toggleMenu} isOpen={isOpen} />
-      <main>{children}</main>
+      <main
+        role="presentation"
+        onMouseMove={() => setIdleTime(0)}
+        onKeyDown={() => setIdleTime(0)}
+      >
+        {children}
+        {idleTime > 8 && (
+          <SlideShow
+            onMouseMove={() => setIdleTime(0)}
+            onKeyDown={() => setIdleTime(0)}
+          ></SlideShow>
+        )}
+      </main>
     </>
   )
 }
---
+++ src/components/slideshow.js
@@ -0,0 +1,60 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Marquee from "react-fast-marquee"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+const query = graphql`
+  query {
+    allContentfulAsset(
+      filter: { file: { contentType: { eq: "image/jpeg" } } }
+    ) {
+      nodes {
+        id
+        gatsbyImageData(placeholder: BLURRED)
+        description
+      }
+    }
+  }
+`
+
+const SlideShow = () => {
+  const data = useStaticQuery(query)
+  function shuffleArray(array) {
+    let curId = array.length
+    // There remain elements to shuffle
+    while (0 !== curId) {
+      // Pick a remaining element
+      let randId = Math.floor(Math.random() * curId)
+      curId -= 1
+      // Swap it with the current element.
+      let tmp = array[curId]
+      array[curId] = array[randId]
+      array[randId] = tmp
+    }
+    return array
+  }
+
+  const imageArray = shuffleArray(data.allContentfulAsset.nodes)
+
+  return (
+    <div className="modal">
+      <Marquee gradient={false} className="image-marquee">
+        {imageArray.map(image => {
+          const imgWidth =
+            (image.gatsbyImageData.width * 80) / image.gatsbyImageData.height
+          return (
+            <div key={image.id} className="marquee-img-container">
+              <GatsbyImage
+                image={image.gatsbyImageData}
+                alt={image.description}
+                style={{ height: "80vh", width: `${imgWidth}vh` }}
+              ></GatsbyImage>
+            </div>
+          )
+        })}
+      </Marquee>
+    </div>
+  )
+}
+
+export default SlideShow
