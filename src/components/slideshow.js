cd198a5183c28fc2435409598a9cd3fb69ab75eb
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2022-09-14 15:05:08 -0400

    remove slideshow placeholder

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -10,7 +10,7 @@ const query = graphql`
     ) {
       nodes {
         id
-        gatsbyImageData(placeholder: BLURRED)
+        gatsbyImageData(placeholder: NONE)
         description
       }
     }
